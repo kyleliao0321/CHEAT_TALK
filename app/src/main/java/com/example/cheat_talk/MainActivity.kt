a5727e818db0eee999a667627d47b3285bdbea8d
Kyle <s8546937623@gmail.com>
2020-04-04 19:24:00 +0200

    init

---
+++ app/src/main/java/com/example/cheat_talk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cheat_talk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
